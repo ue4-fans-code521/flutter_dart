8840207f25c5ea241cf2124d23981e528fb7da3d
ue4-fans-code521 <kongziliang714@163.com>
2020-02-14 09:57:01 +0800

    3-完美点击相应

--- lib/main.dart
+++ lib/main.dart
@@ -16,55 +16,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//定义外围的整体的container
-class AppContainer extends StatelessWidget {
+//下面我们就来实现一个鼠标的点击事件
+
+class AppContainer extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView(
-        children: <Widget>[
-          ContainerItem("Apple-001", "MacBook-P1",
-              "https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg"),
-          ContainerItem("Apple-002", "MacBook-P2",
-              "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imm9u5zj30u00k0adf.jpg"),
-          ContainerItem("Apple-003", "MacBook-P3",
-              "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imqlouhj30u00k00v0.jpg"),
-        ],
-      ),
-    );
+  State createState() {
+    return StateContent();
   }
 }
 
-//具体的有三个item组成的一个作为整体的item
-class ContainerItem extends StatelessWidget {
-//我们需要定义三个成员变量,然后由外部的父组件传递过来这三个参数，进行展示
-  final String title;
-  final String subtitle;
-  final String imgurl;
+class StateContent extends State<AppContainer> {
+  int num = 0;
 
-//提供该类的构造函数
-  ContainerItem(this.title, this.subtitle, this.imgurl);
 
+//我们所需要的交互的小部件全部在该类中实现
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(border: Border.all(width: 3)),
+    return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text(
+                  "点击我+",
+                  style: TextStyle(fontSize: 40),
+                ),
+                color: Colors.green,
+                textColor: Colors.white,
+                onPressed: () {
+                  //在此匿名函数中,来完成对于变量num的修改
+                  print("发生了增加事件");
+                  setState(() {
+                    num++;
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text(
+                  "点击我-",
+                  style: TextStyle(fontSize: 40),
+                ),
+                color: Colors.yellow,
+                textColor: Colors.blue,
+                onPressed: () {
+                  //在此匿名函数中,来完成对于变量num的修改
+                  print("发生了减少事件");
+                  setState(() {
+                    num--;
+                  });
+                },
+              ),
+            ],
+          ),
+
+          //添加实时显示结果的文本控件Text
           Text(
-            title,
-            style: TextStyle(color: Colors.red, fontSize: 20),
-          ),
-          Text(
-            subtitle,
-            style: TextStyle(color: Colors.blue, fontSize: 25),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Image.network(imgurl),
+            "累计:$num",
+            style: TextStyle(fontSize: 40, color: Colors.blue),
+          )
         ],
       ),
     );
